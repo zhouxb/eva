e74658a5bdf1d13feef01f7c1631893fdeaa9be9
zhouxb <zhou.xingbo@gmail.com>
2012-09-05 12:16:13 +0800

    init eva project

---
+++ eva/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls.defaults import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from settings import MEDIA_ROOT
+
+#from django.contrib import admin
+#admin.autodiscover()
+
+urlpatterns = patterns('',
+    #url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': MEDIA_ROOT }),
+
+    # project
+    url(r'account/', include('eva.apps.account.urls')),
+)
+
+urlpatterns += staticfiles_urlpatterns()
+
---
+++ fabfile.py
@@ -0,0 +1,8 @@
+from fabric.api import *
+
+def locale():
+    local('django-admin.py makemessages -l zh_CN -e haml -s')
+
+def build_locale():
+    local('django-admin.py compilemessages')
+
